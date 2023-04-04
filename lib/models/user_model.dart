0ac3380ea0d1c19930dbf6347d6f01f926592c59
braswelljr <braswellkenneth7@gmail.com>
2023-04-04 11:29:42 +0000

    chore: update widgets

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,7 +1,113 @@
 class UserModel {
   final String id;
+  String firstname;
+  String lastname;
+  String othernames;
+  String username;
+  String email;
+  DateTime dateOfBirth;
+  String phone;
+  String address;
+  String city;
+  String country;
+  String role;
+  DateTime createdAt;
+  DateTime updatedAt;
 
   UserModel({
     required this.id,
+    required this.firstname,
+    required this.lastname,
+    required this.othernames,
+    required this.username,
+    required this.email,
+    required this.dateOfBirth,
+    required this.phone,
+    required this.address,
+    required this.city,
+    required this.country,
+    required this.role,
+    required this.createdAt,
+    required this.updatedAt,
   });
+
+  // Convert a UserModel object into a Map object
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'firstname': firstname,
+      'lastname': lastname,
+      'othernames': othernames,
+      'username': username,
+      'email': email,
+      'dateOfBirth': dateOfBirth,
+      'phone': phone,
+      'address': address,
+      'city': city,
+      'country': country,
+      'role': role,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+    };
+  }
+
+  // Convert a Map object into a UserModel object
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      firstname: map['firstname'],
+      lastname: map['lastname'],
+      othernames: map['othernames'],
+      username: map['username'],
+      email: map['email'],
+      dateOfBirth: map['dateOfBirth'],
+      phone: map['phone'],
+      address: map['address'],
+      city: map['city'],
+      country: map['country'],
+      role: map['role'],
+      createdAt: map['createdAt'],
+      updatedAt: map['updatedAt'],
+    );
+  }
+
+  // Convert a Map object into a UserModel object
+  factory UserModel.fromJson(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'],
+      firstname: map['firstname'],
+      lastname: map['lastname'],
+      othernames: map['othernames'],
+      username: map['username'],
+      email: map['email'],
+      dateOfBirth: map['dateOfBirth'],
+      phone: map['phone'],
+      address: map['address'],
+      city: map['city'],
+      country: map['country'],
+      role: map['role'],
+      createdAt: map['createdAt'],
+      updatedAt: map['updatedAt'],
+    );
+  }
+
+  // Convert a UserModel object into a Map object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'firstname': firstname,
+      'lastname': lastname,
+      'othernames': othernames,
+      'username': username,
+      'email': email,
+      'dateOfBirth': dateOfBirth,
+      'phone': phone,
+      'address': address,
+      'city': city,
+      'country': country,
+      'role': role,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+    };
+  }
 }
---
+++ lib/widgets/navbar_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NavbarWidget extends StatefulWidget {
+  const NavbarWidget({super.key});
+
+  @override
+  State<NavbarWidget> createState() => _NavbarWidgetState();
+}
+
+class _NavbarWidgetState extends State<NavbarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: Colors.blue,
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
